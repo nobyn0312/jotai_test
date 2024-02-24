362a62524ae35773fd2c532c5a2b618cb266b162
nobyn0312 <nobyn0312@gmail.com>
2024-02-24 20:46:21 +0900

    testpageで状態を共有したい

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,13 @@
 import "@/styles/globals.css";
+import { Provider } from "jotai";
 import type { AppProps } from "next/app";
-import { atom, useAtom } from 'jotai'
 
-const countAtom = atom(0)
-const [count, setCount] = useAtom(countAtom)
-const handleCountUp = () => setCount(prev => prev + 1);
-
-export default function App({
-  Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <p>{count}</p>
-      <button onClick={handleCountUp}>+</button>
-    </div>
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
   )
+
+
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { atom, useAtom } from 'jotai'
 
 const inter = Inter({ subsets: ["latin"] });
 
+
+const countAtom = atom(0)
+
+
 export default function Home() {
+  const [count, setCount] = useAtom(countAtom)
+  const handleCountUp = () => setCount(prev => prev + 1);
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`container`}
     >
       <h1>jotai</h1>
+      <p>{count}</p>
+      <button onClick={handleCountUp}>+</button>
+
     </main>
   );
 }
